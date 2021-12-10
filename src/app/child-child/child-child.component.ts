ddb298fe6e1e37d456a47db112384965fcd4760e
gilbertorcotes <39133408+gilbertorcotes@users.noreply.github.com>
2021-12-10 12:08:49 -0600

    cambios

---
+++ src/app/child-child/child-child.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child-child',
+  templateUrl: './child-child.component.html',
+  styleUrls: ['./child-child.component.css']
+})
+export class ChildChildComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
